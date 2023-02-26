eb405c6837b5c49c29d4074960d771154c8f40fa
boneyanna23 <boneyanna23@gmail.com>
2023-02-26 17:07:40 -0500

    initial commit

---
+++ Testclass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnitDemonstration;
+namespace Testclass
+
+{
+    public class Testclass
+    {
+        [TestFixture]
+        public class Testcalss1
+        { 
+            [Test]
+            public void  ValidTriangle_Input60and60and60_OutputValidTriangle()
+            { 
+                int firstAngle = 60;
+                int secondAngle =60;
+                int thirdAngle = 60;
+                string expected = "The triangle is valid.";
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                Console.WriteLine(actual);
+                Assert.AreEqual(expected,actual);
+            }
+        }
+    }
+}
